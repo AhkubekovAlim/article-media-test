c744e513569c25c3f876cbb1625be785ee9c10a1
u1028993 <u1028993@server205.hosting.reg.ru>
2020-05-12 01:13:25 +0300

    update contacts

--- about/contacts/index.php
+++ about/contacts/index.php
@@ -1,9 +1,11 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Задайте вопрос");
+$APPLICATION->SetTitle("Контакты");
 ?><p>
- <b>Телефон:</b> <a href="tel:89054363333">8 (905) 436-33-33</a><br>
- <b>Адрес:</b>&nbsp;Нальчик, ул. Кирова, 1д, ТРК Галерея<br>
+ <b>Телефон:</b> <a href="tel:89054363333">8&nbsp;(905)&nbsp;436-33-33</a><br>
+ <b>Адрес:</b> КБР, г.&nbsp;Нальчик, ул.&nbsp;Кирова, д&nbsp;1, ТРК&nbsp;"ГАЛЕРЕЯ", цокольный этаж.<br>
  <b>Instagram:&nbsp;</b><a target="_blank" href="https://www.instagram.com/bambino_official07/">bambino_official07</a><br>
+ <b>Whatsapp:&nbsp;</b><a target="_blank" href="https://api.whatsapp.com/send?phone=89054363333">8&nbsp;(905)&nbsp;436-33-33</a><br>
+ <b>Вконтакте:&nbsp;</b><a target="_blank" href="https://vk.com/bambino_nalchik">bambino_nalchik</a><br>
 </p>
 <div class="mb-2 embed-responsive embed-responsive-16by9">
     <iframe class="embed-responsive-item" width="100%" height="490" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
@@ -16,7 +18,7 @@ $APPLICATION->SetTitle("Задайте вопрос");
      </a>
  </small>
 </div>
-<h2>Задать вопрос</h2>
+<?/*<h2>Задать вопрос</h2>
 	<?$APPLICATION->IncludeComponent(
 	"bitrix:main.feedback",
 	"bootstrap_v4",
@@ -27,4 +29,4 @@ $APPLICATION->SetTitle("Задайте вопрос");
 		"REQUIRED_FIELDS" => array("NAME","EMAIL"),
 		"USE_CAPTCHA" => "Y"
 	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php")?>
+);*/?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php")?>
