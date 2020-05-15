52963046e661ac75a967d81cea8c9a6f2944c38e
u1028993 <u1028993@server205.hosting.reg.ru>
2020-05-15 03:14:14 +0300

    delete unneeded component

--- local/components/article_media/users/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS ['ARTICLE_MEDIA_COMPONENT_USERS_NAME'] = "пользователи";
-$MESS ['ARTICLE_MEDIA_COMPONENT_USERS_DESCRIPTION'] = "пользователи";
-$MESS ['ARTICLE_MEDIA_PARAMETER_USERS'] = "пользователи";
-?>
